069d677703ffc9ac12c56f901a3b383aba686fd4
Henner Kollmann <henner.kollmann@gmx.de>
2018-01-09 11:59:38 +0100

    sort rules

--- index.js
+++ index.js
@@ -1,251 +1,235 @@
 'use strict';
 
 module.exports = {
-	parserOptions: {
-		ecmaVersion: 2016,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-			experimentalObjectRestSpread: true
-		}
-	},
-	globals: {
-    "q": false,
-    "qx": false,
-    "qxWeb": false
-	},
-	env: {
-		node: true,
-		es6: true
-	},
-	plugins: [
-		'qx-rules'
-	],
-	rules: {
-		// Qx-rules plugin
-		'qx-rules/no-refs-in-members': 'error',
-    'qx-rules/no-illegal-private-usage': 'error',
-
-		'comma-dangle': ['error', 'never'],
-		'no-cond-assign': 'error',
-		'no-constant-condition': 'error',
-		'no-control-regex': 'error',
-		'no-debugger': 'error',
-		'no-dupe-args': 'error',
-		'no-dupe-keys': 'error',
-		'no-duplicate-case': 'error',
-		'no-empty-character-class': 'error',
-		'no-empty': ['error', {
-			allowEmptyCatch: true
-		}],
-		'no-ex-assign': 'error',
-		'no-extra-boolean-cast': 'error',
-		// disabled because of https://github.com/eslint/eslint/issues/6028
-		// 'no-extra-parens': [2, 'all', {nestedBinaryExpressions: false}],
-		'no-extra-semi': 'error',
-		'no-func-assign': 'error',
-		'no-inner-declarations': 'error',
-		'no-invalid-regexp': 'error',
-		'no-irregular-whitespace': 'error',
-		'no-obj-calls': 'error',
-		'no-prototype-builtins': 'error',
-		'no-regex-spaces': 'error',
-		'no-sparse-arrays': 'error',
-		'no-template-curly-in-string': 'error',
-		'no-unreachable': 'error',
-		'no-unsafe-finally': 'error',
-		'no-unsafe-negation': 'error',
-		'use-isnan': 'error',
-		'valid-typeof': ['error', {requireStringLiterals: true}],
-		'no-unexpected-multiline': 'error',
-		'accessor-pairs': 'error',
-		'array-callback-return': 'error',
-		'block-scoped-var': 'off',
-		'complexity': 'warn',
-		'curly': 'error',
-		'default-case': 'error',
-		'dot-notation': 0,
-		'dot-location': ['error', 'property'],
-		'eqeqeq': 0,
-		'guard-for-in': 'error',
-		'no-alert': 'error',
-		'no-caller': 'error',
-		'no-case-declarations': 'error',
-		'no-div-regex': 'error',
-		'no-else-return': 'error',
-		'no-empty-pattern': 'error',
-		'no-eq-null': 'error',
-		'no-eval': 'error',
-		'no-extend-native': 'error',
-		'no-extra-bind': 'error',
-		'no-extra-label': 'error',
-		'no-fallthrough': 'error',
-		'no-floating-decimal': 'error',
-		'no-global-assign': 'error',
-		'no-implicit-coercion': 'error',
-		'no-implicit-globals': 'error',
-		'no-implied-eval': 'error',
-		'no-iterator': 'error',
-		'no-labels': 'error',
-		'no-lone-blocks': 'error',
-		'no-loop-func': 0,
-		'no-multi-spaces': 'error',
-		'no-multi-str': 'error',
-		'no-new-func': 'error',
-		'no-new-wrappers': 'error',
-		'no-new': 'error',
-		'no-octal-escape': 'error',
-		'no-octal': 'error',
-		'no-proto': 'error',
-		'no-redeclare': 'error',
-		'no-return-assign': 0,
-		'no-script-url': 'error',
-		'no-self-assign': ['error', {props: true}],
-		'no-self-compare': 'error',
-		'no-sequences': 'error',
-		'no-throw-literal': 'error',
-		'no-unmodified-loop-condition': 'error',
-		'no-unused-expressions': 0,
-		'no-unused-labels': 'error',
-		'no-useless-call': 'error',
-		'no-useless-concat': 'error',
-		'no-useless-escape': 'off',
-		'no-void': 'error',
-		'no-warning-comments': 'warn',
-		'no-with': 'error',
-		'radix': 'error',
-		'wrap-iife': ['error', 'inside'],
-		'yoda': 'error',
-		'no-delete-var': 'error',
-		'no-label-var': 'error',
-		'no-restricted-globals': ['error', 'event'],
-		'no-shadow': 'error',
-		'no-shadow-restricted-names': 'error',
-		'no-undef-init': 'error',
-		'no-undef': ['error', {
-			typeof: true
-		}],
-		'no-unused-vars': 'error',
-		'no-use-before-define': ['error', 'nofunc'],
+    env: {
+        es6: true, node: true
+    }, globals: {
+        'q': false,
+        'qx': false,
+        'qxWeb': false
+    }, parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true, jsx: true
+        }, ecmaVersion: 2016, sourceType: 'module'
+    }, plugins: [
+        'qx-rules'
+    ], rules: {
+		// Special qooxdoo stuff
+		'qx-rules/no-illegal-private-usage': 'error',
+		'qx-rules/no-refs-in-members': 'error', 
+		
+        'accessor-pairs': 'error', 
+		'array-bracket-spacing': ['error', 'never'], 
+		'array-callback-return': 'error', 
+		'arrow-parens': ['error', 'as-needed'], 
+		'arrow-spacing': ['error', {
+            after: true, before: true
+        }], 
+		'block-scoped-var': 'off', 
+		'brace-style': ['error', '1tbs', {
+            allowSingleLine: false
+        }], 
 		// disabled because of https://github.com/eslint/eslint/issues/3420
 		// 'callback-return': ['warn', ['cb', 'callback', 'next', 'done']],
-		'handle-callback-err': 'warn',
+		'camelcase': ['error', {
+            properties: 'always'
+        }], 
+		'comma-dangle': ['error', 'never'], 
+		'comma-spacing': ['error', {
+            after: true, before: false
+        }], 
+		'comma-style': ['error', 'last'], 
+		'complexity': 'warn', 
+		'computed-property-spacing': ['error', 'never'], 
+		'constructor-super': 'error', 
+		'curly': 'error', 
+		'default-case': 'error', 
+		'dot-location': ['error', 'property'], 
+		'dot-notation': 0, 
+		'eol-last': 'error', 
+		'eqeqeq': 0, 
+		'func-call-spacing': ['error', 'never'], 
+		'func-names': ['error', 'never'], 
+		'generator-star-spacing': ['error', 'both'], 
+		'guard-for-in': 'error', 
+		'handle-callback-err': 'warn', 
+		'indent': ['error', 2, {
+            SwitchCase: 1
+        }], 
+		'jsx-quotes': 'error', 
+		'key-spacing': 0, 
+		'keyword-spacing': 'error', 
+		'linebreak-style': ['error', 'unix'], 
+		'max-depth': 'warn', 
+		'max-lines': ['warn', {
+            max: 1024
+        }], 
+		'max-nested-callbacks': ['warn', 4], 
+		'max-params': ['warn', {
+            max: 4
+        }], 
+		'max-statements-per-line': 'error', 
+		'new-cap': ['error', {
+            capIsNew: true, newIsCap: true
+        }], 
+		'new-parens': 'error', 
+		'no-alert': 'error', 
+		'no-array-constructor': 'error', 
+		'no-caller': 'error', 
+		'no-case-declarations': 'error', 
+		'no-class-assign': 'error', 
+		'no-cond-assign': 'error', 
+		'no-const-assign': 'error', 
+		'no-constant-condition': 'error', 
+		'no-control-regex': 'error', 
+		'no-debugger': 'error', 
+		'no-delete-var': 'error', 
+		'no-div-regex': 'error', 
+		'no-dupe-args': 'error', 
+		'no-dupe-class-members': 'error', 
+		'no-dupe-keys': 'error', 
+		'no-duplicate-case': 'error', 
+		'no-duplicate-imports': ['error', {
+            includeExports: true
+        }], 
+		'no-else-return': 'error', 
+		'no-empty': ['error', {
+            allowEmptyCatch: true
+        }], 
+		'no-empty-character-class': 'error', 
+		'no-empty-pattern': 'error', 
+		'no-eq-null': 'error', 
+		'no-eval': 'error', 
+		'no-ex-assign': 'error', 
+		'no-extend-native': 'error', 
+		'no-extra-bind': 'error', 
+		'no-extra-boolean-cast': 'error', 
+		'no-extra-label': 'error', 
+		// disabled because of https://github.com/eslint/eslint/issues/6028
+		// 'no-extra-parens': [2, 'all', {nestedBinaryExpressions: false}],
+		'no-extra-semi': 'error', 
+		'no-fallthrough': 'error', 
+		'no-floating-decimal': 'error', 
+		'no-func-assign': 'error', 
+		'no-global-assign': 'error', 
+		'no-implicit-coercion': 'error', 
+		'no-implicit-globals': 'error', 
+		'no-implied-eval': 'error', 
+		'no-inner-declarations': 'error', 
+		'no-invalid-regexp': 'error', 
+		'no-irregular-whitespace': 'error', 
+		'no-iterator': 'error', 
+		'no-label-var': 'error', 
+		'no-labels': 'error', 
+		'no-lone-blocks': 'error', 
+		'no-lonely-if': 'error', 
+		'no-loop-func': 0, 
+		'no-mixed-operators': 'error', 
 		'no-mixed-requires': ['error', {
-			grouping: true,
-			allowCall: true
-		}],
-		'no-new-require': 'error',
-		'no-path-concat': 'error',
+            allowCall: true, grouping: true
+        }], 
+		'no-mixed-spaces-and-tabs': 'error', 
+		'no-multi-spaces': 'error', 
+		'no-multi-str': 'error', 
+		'no-multiple-empty-lines': ['off', {
+            max: 1
+        }], 
+		'no-negated-condition': 'error', 
+		'no-nested-ternary': 'warn', 
+		'no-new': 'error', 
+		'no-new-func': 'error', 
+		'no-new-object': 'error', 
+		'no-new-require': 'error', 
+		'no-new-symbol': 'error',
+		'no-new-wrappers': 'error', 
+		'no-obj-calls': 'error', 
+		'no-octal': 'error',
+		'no-octal-escape': 'error', 
+		'no-path-concat': 'error', 
+		'no-proto': 'error',
+		'no-prototype-builtins': 'error',
+		'no-redeclare': 'error', 
+		'no-regex-spaces': 'error', 
+		'no-restricted-globals': ['error', 'event'],
 		'no-restricted-imports': ['error', 'domain', 'freelist', 'smalloc', 'sys', 'colors'],
 		'no-restricted-modules': ['error', 'domain', 'freelist', 'smalloc', 'sys', 'colors'],
-		'array-bracket-spacing': ['error', 'never'],
-		'brace-style': ['error', '1tbs', {
-			allowSingleLine: false
-		}],
-		'camelcase': ['error', {
-			properties: 'always'
-		}],
-		'comma-spacing': ['error', {
-			before: false,
-			after: true
-		}],
-		'comma-style': ['error', 'last'],
-		'computed-property-spacing': ['error', 'never'],
-		'eol-last': 'error',
-		'func-call-spacing': ['error', 'never'],
-		'func-names': ['error', 'never'],
-		'indent': ['error', 2, {
-			SwitchCase: 1
-		}],
-		'jsx-quotes': 'error',
-		'key-spacing': 0,
-		'keyword-spacing': 'error',
-		'linebreak-style': ['error', 'unix'],
-		'max-depth': 'warn',
-		'max-lines': ['warn', {
-			max: 1024
-		}],
-		'max-nested-callbacks': ['warn', 4],
-		'max-params': ['warn', {
-			max: 4
-		}],
-		'max-statements-per-line': 'error',
-		'new-cap': ['error', {
-			newIsCap: true,
-			capIsNew: true
-		}],
-		'new-parens': 'error',
-		'no-array-constructor': 'error',
-		'no-lonely-if': 'error',
-		'no-mixed-operators': 'error',
-		'no-mixed-spaces-and-tabs': 'error',
-		'no-multiple-empty-lines': ['off', {
-			max: 1
-		}],
-		'no-nested-ternary': 'warn',
-		'no-negated-condition': 'error',
-		'no-new-object': 'error',
-		'no-restricted-syntax': ['error', 'WithStatement'],
-		'no-whitespace-before-property': 'error',
-		'no-trailing-spaces': 'error',
+		'no-restricted-syntax': ['error', 'WithStatement'], 
+		'no-return-assign': 0, 
+		'no-script-url': 'error', 
+		'no-self-assign': ['error', {props: true}], 
+		'no-self-compare': 'error', 
+		'no-sequences': 'error',
+		'no-shadow': 'error', 
+		'no-shadow-restricted-names': 'error', 
+		'no-sparse-arrays': 'error',
+		'no-template-curly-in-string': 'error',
+		'no-this-before-super': 'error',
+		'no-throw-literal': 'error', 
+		'no-trailing-spaces': 'error', 
+		'no-undef': ['error', {
+            typeof: true
+        }], 
+		'no-undef-init': 'error',
+		'no-unexpected-multiline': 'error', 
+		'no-unmodified-loop-condition': 'error',
 		'no-unneeded-ternary': 'error',
-		'object-curly-spacing': ['error', 'never'],
-		// disabled because of https://github.com/sindresorhus/eslint-config-xo/issues/27
-		// 'object-property-newline': 'error',
-		'one-var': ['error', 'never'],
-		'one-var-declaration-per-line': 'error',
+		'no-unreachable': 'error', 
+		'no-unsafe-finally': 'error',
+		'no-unsafe-negation': 'error',
+		'no-unused-expressions': 0, 
+		'no-unused-labels': 'error', 
+		'no-unused-vars': 'error', 
+		'no-use-before-define': ['error', 'nofunc'],
+		'no-useless-call': 'error', 
+		'no-useless-computed-key': 'error', 
+		'no-useless-concat': 'error', 
+		'no-useless-constructor': 'error',
+		'no-useless-escape': 'off', 
+		'no-useless-rename': 'error', 
+		'no-void': 'error', 
+		'no-warning-comments': 'warn', 
+		'no-whitespace-before-property': 'error', 
+		'no-with': 'error', 
+		'object-curly-spacing': ['error', 'never'], 
+		'one-var': ['error', 'never'], 
+		'one-var-declaration-per-line': 'error', 
 		'operator-assignment': ['error', 'always'],
-		'operator-linebreak': ['error', 'after'],
-		'padded-blocks': ['error', 'never'],
+		'operator-linebreak': ['error', 'after'], 
+		'object-property-newline': 'error',
+		'padded-blocks': ['error', 'never'], 
 		'quote-props': ['error', 'consistent-as-needed'],
 		'quotes': ['error', 'double', {
-			allowTemplateLiterals: true
-		}],
-		'semi-spacing': ['error', {
-			before: false,
-			after: true
-		}],
+            allowTemplateLiterals: true
+        }], 
+		'radix': 'error', 
+		'require-yield': 'error', 
+		'rest-spread-spacing': ['error', 'never'], 
 		'semi': ['error', 'always'],
+		'semi-spacing': ['error', {
+            after: true, before: false
+        }], 
 		'space-before-blocks': ['error', 'always'],
 		'space-before-function-paren': ['error', {
-			anonymous: 'never',
-			named: 'never'
-		}],
-		'space-in-parens': ['error', 'never'],
-		'space-infix-ops': 0,
+            anonymous: 'never',
+            named: 'never'
+        }],
+		'space-in-parens': ['error', 'never'], 
+		'space-infix-ops': 0, 
 		'space-unary-ops': 'error',
 		'spaced-comment': ['error', 'always', {
-			line: {
-				exceptions: ['-']
-			},
-			block: {
-				markers: ['!'],
-				balanced: true
-			}
-		}],
-		'unicode-bom': ['error', 'never'],
-		'arrow-parens': ['error', 'as-needed'],
-		'arrow-spacing': ['error', {
-			before: true,
-			after: true
-		}],
-		'constructor-super': 'error',
-		'generator-star-spacing': ['error', 'both'],
-		'no-class-assign': 'error',
-		'no-const-assign': 'error',
-		'no-dupe-class-members': 'error',
-		'no-duplicate-imports': ['error', {
-			includeExports: true
-		}],
-		'no-new-symbol': 'error',
-		'no-this-before-super': 'error',
-		'no-useless-computed-key': 'error',
-		'no-useless-constructor': 'error',
-		'no-useless-rename': 'error',
-		'require-yield': 'error',
-		'rest-spread-spacing': ['error', 'never'],
-		'symbol-description': 'error',
-		'template-curly-spacing': 'error',
-		'yield-star-spacing': ['error', 'both']
-	}
+            block: {
+                balanced: true, markers: ['!']
+            }, line: {
+                exceptions: ['-']
+            }
+        }],
+		'symbol-description': 'error', 
+		'template-curly-spacing': 'error', 
+		'unicode-bom': ['error', 'never'], 
+		'use-isnan': 'error', 
+		'valid-typeof': ['error', {requireStringLiterals: true}], 
+		'wrap-iife': ['error', 'inside'], 
+		'yield-star-spacing': ['error', 'both'], 
+		'yoda': 'error'
+    }
 };
